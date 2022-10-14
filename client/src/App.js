9c5811349be1317a51a3ef85cae90ccc563d0a14
KutenkovV <kutenkovvlad017@gmail.com>
2022-10-14 21:40:09 +0800

    Развернул проект. Сделал структуру.

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./pages/main";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/pages/main.js
@@ -0,0 +1,13 @@
+function name() {
+  document.title = "Главная страница";
+
+  return (
+    <>
+      <div>
+        Тут будет контент
+      </div>
+    </>
+  );
+}
+
+export default name;
