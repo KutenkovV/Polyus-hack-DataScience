7a304b41dbc8ecad102e76de9ab6189460f32c06
KutenkovV <kutenkovvlad017@gmail.com>
2022-10-15 23:49:22 +0800

    Таблица. Проблемы с выводом. Закомментил проблемный код

--- client/src/components/analysis.js
+++ client/src/components/analysis.js
@@ -1,12 +1,25 @@
-import '../components/analysis.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "../components/analysis.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 
 const Analysis = ({ data }) => {
+  const [dataa, setData] = useState([]);
+
+  useEffect(() => {
+    setData([data]);
+  }, []);
+
   return (
     <>
       <div>
         <label className="form-label">Введите размер негабарита</label>
-        <input style={{ width: "50%"}} className="form-control" type="number" min="0" step="1"></input>
+        <input
+          style={{ width: "50%" }}
+          className="form-control"
+          type="number"
+          min="0"
+          step="1"
+        ></input>
       </div>
       <div className="pt-2">
         <label className="form-label">Гранулометрический состав руды</label>
@@ -19,41 +32,12 @@ const Analysis = ({ data }) => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>более 250</td>
-              <td>с бэка получаем</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>от 150 до 250</td>
-              <td>с бэка получаем</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>от 100 до 150</td>
-              <td>с бэка получаем</td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>от 80 до 100</td>
-              <td>с бэка получаем</td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td>от 70 до 80</td>
-              <td>с бэка получаем</td>
-            </tr>
-            <tr>
-              <td>6</td>
-              <td>от 40 до 70</td>
-              <td>с бэка получаем</td>
-            </tr>
-            <tr>
-              <td>7</td>
-              <td>от 0 до 40</td>
-              <td>с бэка получаем</td>
-            </tr>
+            {/* Не придумал как другие атрибуты выводить по колонкам */}
+            {/* {dataa.map((item) => (
+              <tr>
+                <td>{item.propertyes}</td>
+              </tr>
+            ))} */}
           </tbody>
         </table>
       </div>
--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -6,13 +6,30 @@ import "@progress/kendo-theme-material/dist/all.css";
 import Circular from "../charts/Circular";
 import MaxSize from "../charts/MaxSize";
 import ChartOne from "../charts/ChartOne";
+import axios from "axios";
+
 
 import io from "socket.io-client";
 // const socket = io("http://127.0.0.1:5000/");
 
 const Main = () => {
-  const [analysis, setAnalysis] = useState([]);
-  const [maxSize, setMaxSize] = useState([]);
+  // Для сокетов если хоть кто-то сделает их на бэке блин
+  // const [analysis, setAnalysis] = useState([]);
+  // const [maxSize, setMaxSize] = useState([]);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    console.log("Axios is worked!");
+    axios
+      .get("http://127.0.0.1:5000/get-image")
+      .then((response) => {
+        setData(response.data);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   // useEffect(() => {
   //   // В таблицу с гранулометрическим анализом
@@ -31,12 +48,12 @@ const Main = () => {
       <div className="m-1 d-flex">
         <div className="col-6 content_info">
           <Video />
-          <MaxSize data={maxSize} />
+          <MaxSize data={data} />
         </div>
 
         <div className="d-flex ms-3 col content_info">
           <div className="d-block col">
-            <Analysis data={analysis} />
+            <Analysis data={data} />
             <Circular />
             {/* <ChartOne /> */}
           </div>
@@ -44,6 +61,6 @@ const Main = () => {
       </div>
     </>
   );
-}
+};
 
 export default Main;
