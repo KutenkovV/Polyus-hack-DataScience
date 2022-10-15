eeb06c63dc0a2719d9f29a7effd8a99036224263
PaaAngk <paaAngarsk@yandex.ru>
2022-10-15 16:22:53 +0800

    sre

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import sqlite3 as sql
 from flask_socketio import SocketIO, send, emit
 from flask_cors import CORS
-from flask import jsonify, json
+from flask import jsonify, json, send_file
 
 #ML
 # import matplotlib.pyplot as plt
@@ -30,13 +30,15 @@ import torch
 import numpy as np
 import torch.backends.cudnn as cudnn
 from numpy import random
-
+from PIL import Image
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+import pickle
+import base64
 
 
 def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
@@ -89,7 +91,6 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 # _ = model.eval()
 classes_to_filter = None  #You can give list of classes to filter by name, Be happy you don't have to put class number. ['train','person' ]
 opt  = {
-    
     "weights": "model/best.pt", # Path to weights file default weights are for nano model
     "yaml"   : "model/hyp.yaml",
     "img-size": 1280, # default image size
@@ -179,57 +180,75 @@ def list():
     return {'rows': results}
 
 
+def im2json(im):
+    """Convert a Numpy array to JSON string"""
+    imdata = pickle.dumps(im)
+    jstr = json.dumps({"image": base64.b64encode(imdata).decode('ascii')})
+    return jstr
 
 ####################################################
 @ app.route('/get-image')
 def model():
-    
-    names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
-    if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))
+    with torch.no_grad():
+        weights, imgsz = opt['weights'], opt['img-size']
+        set_logging()
+        device = select_device(opt['device'])
+        half = device.type != 'cpu'
+        model = attempt_load(weights, map_location=device)  # load FP32 model
+        stride = int(model.stride.max())  # model stride
+        imgsz = check_img_size(imgsz, s=stride)  # check img_size
+        if half:
+            model.half()
+        
+        names = model.module.names if hasattr(model, 'module') else model.names
+        colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+        if device.type != 'cpu':
+            model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))
 
-    img0 = cv2.imread(source_image_path)
-    img = letterbox(img0, imgsz, stride=stride)[0]
-    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-    img = np.ascontiguousarray(img)
-    img = torch.from_numpy(img).to(device)
-    img = img.half() if half else img.float()  # uint8 to fp16/32
-    img /= 255.0  # 0 - 255 to 0.0 - 1.0
-    if img.ndimension() == 3:
-        img = img.unsqueeze(0)
+        img0 = cv2.imread(source_image_path)
+        img = letterbox(img0, imgsz, stride=stride)[0]
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img)
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
 
-    # Inference
-    t1 = time_synchronized()
-    predict = model(img, augment= False)[0]
+        # Inference
+        t1 = time_synchronized()
+        predict = model(img, augment= False)[0]
 
-    # Apply NMS
-    classes = None
-    if opt['classes']:
-        classes = []
-        for class_name in opt['classes']:
-            classes.append(opt['classes'].index(class_name))
+        # Apply NMS
+        classes = None
+        if opt['classes']:
+            classes = []
+            for class_name in opt['classes']:
+                classes.append(opt['classes'].index(class_name))
 
 
-    pred = non_max_suppression(predict, opt['conf-thres'], opt['iou-thres'], classes= classes, agnostic= False)
-    t2 = time_synchronized()
-    for i, det in enumerate(pred):
-        s = ''
-        s += '%gx%g ' % img.shape[2:]  # print string
-        gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]
-        if len(det):
-            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+        pred = non_max_suppression(predict, opt['conf-thres'], opt['iou-thres'], classes= classes, agnostic= False)
+        t2 = time_synchronized()
+        for i, det in enumerate(pred):
+            s = ''
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]
+            if len(det):
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
 
-        for c in det[:, -1].unique():
-            n = (det[:, -1] == c).sum()  # detections per class
-            s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+                s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
-        for *xyxy, conf, cls in reversed(det):
-            label = f'{names[int(cls)]} {conf:.2f}'
-            plot_one_box(xyxy, img0, label=label, color=colors[int(cls)], line_thickness=3)
- 
+            for *xyxy, conf, cls in reversed(det):
+                label = f'{names[int(cls)]} {conf:.2f}'
+                plot_one_box(xyxy, img0, label=label, color=colors[int(cls)], line_thickness=3)
 
-    return jsonify(img0)
+    # _, img_encoded = cv2.imencode('.jpg', img0)
+
+    return im2json(img0)
+    #return send_file(im2json(img0), mimetype='image/png')
+    # return jsonify(img_encoded)
 
 
 
--- client/src/components/video.js
+++ client/src/components/video.js
@@ -1,10 +1,26 @@
 import "../components/video.css";
+import { useEffect, useState } from 'react';
+
 
 const Video = () => {
+  const axios = require('axios').default;
+  const [data, setData] = useState()
+  axios.get('http://127.0.0.1:5000/get-image')
+    .then(function (response) {
+      setData(response);
+      console.log(response)
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+    });
   return (
-    <video controls width="100%" height="50%">
-      <source src="/" type="video/mp4" />
-    </video>
+    // <video controls width="100%" height="50%">
+    //   <source src="/" type="video/mp4" />
+    // </video>
+    
+    <img src={`data:image/png;base64,${data}`} alt="Red dot" />
   );
 };
 
