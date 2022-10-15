3add6e66d49ebfd15afe749e6aeea8fb666b6916
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-10-15 10:59:19 +0800

    fix packages and try socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,45 @@
-import "./App.css"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Main from "./pages/main";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './pages/main';
+
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+
+const socket = io('http://127.0.0.1:5000/');
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('message', () => {
+      setLastPong(new Date().toISOString());
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('pong');
+    };
+  }, []);
+
+  const sendPing = () => {
+    socket.emit('my_event');
+  };
+
   return (
     <div className="content">
+      <p>Connected: {'' + isConnected}</p>
+      <p>Last pong: {lastPong || '-'}</p>
+      <button onClick={sendPing}>Send ping</button>
       <Router>
         <Routes>
           <Route path="/" element={<Main />} />
--- client/src/components/analysis.js
+++ client/src/components/analysis.js
@@ -1,16 +1,16 @@
-import "../components/analysis.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import '../components/analysis.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Analysis = () => {
   return (
     <>
       <div>
-        <label class="form-label">Введите размер негабарита</label>
-        <input class="form-control" type="number" min="0" step="1"></input>
+        <label className="form-label">Введите размер негабарита</label>
+        <input className="form-control" type="number" min="0" step="1"></input>
       </div>
       <div className="pt-2">
-        <label class="form-label">Гранулометрический состав руды</label>
-        <table class="table">
+        <label className="form-label">Гранулометрический состав руды</label>
+        <table className="table">
           <thead>
             <tr>
               <th>Легенда</th>
