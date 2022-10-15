df3c87ecfa5c8549c04e9337e4d804fbe0a2ce57
PaaAngk <paaAngarsk@yandex.ru>
2022-10-15 22:40:04 +0800

    aaa

--- backend/app.py
+++ backend/app.py
@@ -66,14 +66,14 @@ import base64
 #     ratio = r, r  # width, height ratios
 #     new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
 #     dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - \
-        new_unpad[1]  # wh padding
+    #    new_unpad[1]  # wh padding
 #     if auto:  # minimum rectangle
 #         dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
 #     elif scaleFill:  # stretch
 #         dw, dh = 0.0, 0.0
 #         new_unpad = (new_shape[1], new_shape[0])
 #         ratio = new_shape[1] / shape[1], new_shape[0] / \
-            shape[0]  # width, height ratios
+      #      shape[0]  # width, height ratios
 
 #     dw /= 2  # divide padding into 2 sides
 #     dh /= 2
