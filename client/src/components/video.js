d3c1d6fb4c7a70c06ae1198346f4bb2776f9859c
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-10-15 18:28:19 +0800

    import axios

--- client/src/components/video.js
+++ client/src/components/video.js
@@ -1,35 +1,30 @@
-import "../components/video.css";
+import '../components/video.css';
 import { useEffect, useState } from 'react';
-
-
+import axios from 'axios';
 const Video = () => {
-  const axios = require('axios').default;
   const [data, setData] = useState();
-  const handleSub = async () => {
-
-  };
+  const handleSub = async () => {};
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:5000/get-image')
-    .then(function (response) {
-      setData(response.data.image);
-      console.log(response)
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .then(function () {
-    });
-    console.log("!!!!!!!!!!!!!!!")
-  } , []);
+    axios
+      .get('http://127.0.0.1:5000/get-image')
+      .then(function (response) {
+        setData(response.data.image);
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {});
+    console.log('!!!!!!!!!!!!!!!');
+  }, []);
 
-  
   return (
     // <video controls width="100%" height="50%">
     //   <source src="/" type="video/mp4" />
     // </video>
-    
-    <img onChange={handleSub} src={`data:image/png;base64, ${data}`} alt="Red dot" />
+
+    <img onChange={handleSub} width="100%" src={`data:image/png;base64, ${data}`} alt="Red dot" />
   );
 };
 
